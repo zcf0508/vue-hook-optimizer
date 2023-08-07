92e6753b27caf118beac8b69a194bf5baa2e5a6d
zcf0508 <zcf0508@live.com>
2023-08-07 23:53:59 +0800

    chore: init playground

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,51 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: process.cwd(),
-    project: ['tsconfig.json'],
+    project: ['tsconfig.json', './playground/tsconfig.json'],
+    extraFileExtensions: ['.vue'],
   },
   plugins: [
+    'vue',
     '@typescript-eslint',
+  ],
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      extends: [
+        'plugin:vue/vue3-recommended',
+      ],
+      rules: {
+        'vue/multi-word-component-names': [0],
+        'vue/max-attributes-per-line': ['error', {
+          'singleline': {
+            'max': 3,
+          },      
+          'multiline': {
+            'max': 1,
+          },
+        }],
+        'vue/html-self-closing': [0, {
+          'html': {
+            'void': 'never',
+            'normal': 'always',
+            'component': 'always',
+          },
+          'svg': 'always',
+          'math': 'always',
+        }],
+        'vue/html-indent': ['error', 2, {
+          'attribute': 1,
+          'baseIndent': 1,
+          'closeBracket': 0,
+          'alignAttributesVertically': true,
+          'ignores': [],
+        }],
+      },
+    },
   ],
   rules: {
     semi: ['error', 'always'],
