942e217c6da2202516be519cbc0cb085cf7116b4
zcf0508 <zcf0508@live.com>
2023-08-07 11:49:30 +0800

    fix: Refactor analyze function to handle function declarations

--- src/analyze/setupScript.ts
+++ src/analyze/setupScript.ts
@@ -21,15 +21,23 @@ export function analyze(
   traverse(ast, {
     Identifier(path) {
       const name = path.node.name;
+      const scopeParent = path.scope.parent;
       const binding = path.scope.getBinding(name);
 
       if (binding) {
-        if (['const', 'let', 'var'].includes(binding.kind)) {
+        if (!scopeParent && ['const', 'let', 'var'].includes(binding.kind)) {
           graph.nodes.add(name);
           if(!graph.edges.get(name)) {
             graph.edges.set(name, new Set());
           }
-        } else if (binding.kind === 'hoisted') {
+        }
+      }
+    },
+    FunctionDeclaration(path) {
+      const name = path.node.id?.name
+      if(name) {
+        const binding = path.scope.getBinding(name);
+        if(binding && path.parent.type === 'Program') {
           graph.nodes.add(name);
           if(!graph.edges.get(name)) {
             graph.edges.set(name, new Set());
@@ -41,9 +49,8 @@ export function analyze(
 
   traverse(ast, {
     FunctionDeclaration(path) {
-      if(graph.nodes.has(path.node.id.name)) {
-        const name = path.node.id.name;
-        console.log(name);
+      const name = path.node.id?.name;
+      if(name && graph.nodes.has(name)) {
         path.traverse({
           Identifier(path) {
             if(graph.nodes.has(path.node.name) && path.node.name !== name) {
@@ -57,27 +64,18 @@ export function analyze(
     // get the relation between the variable and the function
     VariableDeclarator(path) {
       if(path.node.init) {
-        if(path.node.init.type === 'ArrowFunctionExpression') {
+        if(['CallExpression', 'ArrowFunctionExpression', 'FunctionDeclaration'].includes(path.node.init.type)) {
           //  @ts-ignore
-          const name = path.node.id.name;
-          path.traverse({
-            Identifier(path) {
-              if(graph.nodes.has(path.node.name) && path.node.name !== name) {
-                graph.edges.get(name)?.add(path.node.name);
-              }
-            },
-          });
-        }
-        if(path.node.init.type === 'CallExpression') {
-          //  @ts-ignore
-          const name = path.node.id.name;
-          path.traverse({
-            Identifier(path) {
-              if(graph.nodes.has(path.node.name) && path.node.name !== name) {
-                graph.edges.get(name)?.add(path.node.name);
-              }
-            },
-          });
+          const name = path.node.id?.name;
+          if(name && graph.nodes.has(name)) {
+            path.traverse({
+              Identifier(path) {
+                if(graph.nodes.has(path.node.name) && path.node.name !== name) {
+                  graph.edges.get(name)?.add(path.node.name);
+                }
+              },
+            });
+          }
         }
       }
     },
