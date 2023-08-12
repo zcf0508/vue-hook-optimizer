cc6520a1bc8d955fbe347159258da1229d49ffc3
zcf0508 <zcf0508@live.com>
2023-08-12 17:36:37 +0800

    feat: add resize

--- playground/app.vue
+++ playground/app.vue
@@ -1,13 +1,45 @@
+<template>
+  <div
+    ref="container"
+    class="w-full h-full flex"
+    @mousemove="dragMove"
+    @mouseup="dragEnd"
+    @mouseleave="dragEnd"
+  >
+    <div
+      class="flex-1 border-solid border-0 border-r-1"
+      :style="`max-width: ${boundSplit}%`"
+    >
+      <ClientOnly>
+        <CodeMirror :value="code" @change="codeChange"></CodeMirror>
+      </ClientOnly>
+    </div>
+    <div 
+      ref="dragger"
+      class="px-1 flex-shrink flex items-center cursor-ew-resize" 
+      @mousedown.prevent="dragStart"
+    >
+      |
+    </div>
+    <div class="flex-1">
+      <div>
+        <button @click="start">
+          analyze
+        </button>
+      </div>
+      <div ref="networkRef" class="h-full"></div>
+    </div>
+  </div>
+</template>
+
 <script lang="ts" setup>
 import * as vis from 'vis-network';
 import CodeMirror from './components/codemirror/CodeMirror.vue';
 import { defaultCode } from './default-code';
-import { 
-  getVisData,
-} from './vis';
 
 const code = ref(defaultCode);
 
+provide('autoresize', true);
 
 function codeChange(value: string) {
   code.value = value;
@@ -51,24 +83,44 @@ onMounted(() => {
   });
 });
 
+// --resize
+
+const container = ref();
+
+const state = reactive({
+  dragging: false,
+  split: 50,
+});
+
+const boundSplit = computed(() => {
+  const { split } = state;
+  return split < 10 ? 10 : split > 50 ? 50 : split;
+});
+
+let startPosition = 0;
+let startSplit = 0;
+
+function dragStart(e: MouseEvent) {
+  state.dragging = true;
+  startPosition = e.pageX;
+  startSplit = boundSplit.value;
+}
+
+function dragMove(e: MouseEvent) {
+  if (state.dragging) {
+    const position = e.pageX;
+    const totalSize = container.value.offsetWidth;
+    const dp = position - startPosition;
+    state.split = startSplit + ~~((dp / totalSize) * 100);
+  }
+}
+
+function dragEnd() {
+  state.dragging = false;
+}
+
 </script>
-<template>
-  <div class="w-full h-full flex">
-    <div class="flex-1">
-      <ClientOnly>
-        <CodeMirror :value="code" @change="codeChange"></CodeMirror>
-      </ClientOnly>
-    </div>
-    <div class="flex-1">
-      <div>
-        <button @click="start">
-          analyze
-        </button>
-      </div>
-      <div ref="networkRef" class="h-full"></div>
-    </div>
-  </div>
-</template>
+
 <style>
 html, body, #__nuxt {
   margin: 0;
