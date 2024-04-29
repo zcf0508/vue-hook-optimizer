c4bf159ace061fffcfd837e0642753f62993caea
zcf0508 <zcf0508@live.com>
2024-04-29 18:46:49 +0800

    chore: fix coverage test fail

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
     globals: true,
     coverage: {
+      include: ['src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
       provider: 'istanbul',
       reporter: ['text'],
     },
