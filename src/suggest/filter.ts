46a3f054292a753d67f9e0d74b4e70b9934e34a0
zcf0508 <zcf0508@live.com>
2024-07-12 18:06:46 +0800

    fix: variables and functions should be included

--- src/suggest/filter.ts
+++ src/suggest/filter.ts
@@ -82,7 +82,7 @@ export function findLinearPaths(graph: Map<TypedNode, Set<TypedNode>>) {
   const visitedNodes = new Set();
 
   for (const [node, edges] of graph.entries()) {
-    if (edges.size === 1 && !visitedNodes.has(node) && node.type === NodeType.fun) {
+    if (edges.size === 1 && !visitedNodes.has(node)) {
       const path = [node];
       let nextNode = Array.from(edges)[0];
 
